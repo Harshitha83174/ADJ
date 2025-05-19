19a4e4f07d7020a924a10ce96f1f1712eb667ec8
Harshitha M <153185218+Harshitha83174@users.noreply.github.com>
2025-05-19 22:35:20 +0530

    Add files via upload

---
+++ WhitespaceRemover.java
@@ -0,0 +1,31 @@
+package Lab3;
+import java.util.Scanner;
+
+public class WhitespaceRemover {
+
+    // User-defined function to remove all whitespace characters
+    public static String removeWhitespace(String str) {
+        if (str == null) {
+            return null;
+        }
+        // \s matches all whitespace characters (space, tab, newline, etc.)
+        return str.replaceAll("\\s+", "");
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Input string from user
+        System.out.print("Enter a string with whitespace: ");
+        String input = scanner.nextLine();
+
+        // Remove whitespace using the function
+        String noWhitespace = removeWhitespace(input);
+
+        // Display result
+        System.out.println("String without whitespace: '" + noWhitespace + "'");
+
+        scanner.close();
+    }
+}
+
